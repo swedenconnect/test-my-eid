c921c96172dda30cdc7cd0ad737d6ed6ed5ab45f
Martin Lindström <martin.lindstrom@litsec.se>
2019-01-17 14:25:49 +0100

    Added endpoint for direct communication with the eIDAS connector.
    The endpoint /eidas directs the user directly to the eIDAS connector
    and the endpoint /eidas/XX directs the user to the eIDAS connector with
    information about the desired country (thus by-passing the connector
    discovery page).

--- src/main/java/se/swedenconnect/eid/sp/controller/SpController.java
+++ src/main/java/se/swedenconnect/eid/sp/controller/SpController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Sweden Connect
+ * Copyright 2018-2019 Sweden Connect
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,6 +49,10 @@ public class SpController extends BaseController {
   @Autowired
   private IdpListConfiguration idpListConfiguration;
 
+  /** The entityID for the eIDAS connector. */
+  @Value("${sp.eidas-connector.entity-id}")
+  private String eidasConnectorEntityId;
+
   /**
    * Controller method for the home endpoint.
    * 
@@ -63,12 +69,44 @@ public class SpController extends BaseController {
     mav.addObject("debug", debug);
     mav.addObject("idpList", this.idpListConfiguration.getIdps()
       .stream()
-      .filter(idp -> device.isNormal() || idp.isMobileUse()) 
+      .filter(idp -> device.isNormal() || idp.isMobileUse())
       .map(i -> i.getIdpModel(LocaleContextHolder.getLocale()))
       .collect(Collectors.toList()));
 
     log.trace("Adding IdPs {}", this.idpListConfiguration.getIdps());
     return mav;
+  }
+
+  /**
+   * Controller that by-passes the discovery page and goes directly to the eIDAS connector.
+   * 
+   * @param debug
+   *          debug flag
+   * @return a redirect string to the SAML request endpoint
+   */
+  @GetMapping("/eidas")
+  public ModelAndView eidas(
+      @RequestParam(value = "debug", required = false, defaultValue = "false") Boolean debug) {
+
+    return new ModelAndView(String.format("redirect:/saml2/request?selectedIdp=%s&debug=%s", this.eidasConnectorEntityId, debug));
+  }
+
+  /**
+   * Controller that by-passed the discovery page and goes directly to the eIDAS connector with a pre-selected country.
+   * 
+   * @param country
+   *          the two-letter country code for the country to send the request from the connector to
+   * @param debug
+   *          debug flag
+   * @return a redirect string to the SAML request endpoint
+   */
+  @GetMapping("/eidas/{country}")
+  public ModelAndView eidasCountry(
+      @PathVariable(value = "country", required = true) String country,
+      @RequestParam(value = "debug", required = false, defaultValue = "false") Boolean debug) {
+
+    return new ModelAndView(String.format("redirect:/saml2/request?selectedIdp=%s&country=%s&debug=%s", 
+      this.eidasConnectorEntityId, country, debug));
   }
 
   /**
