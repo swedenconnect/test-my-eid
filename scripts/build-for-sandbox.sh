eceb74ea5c721351099274b232a1060b342dff44
Martin Lindström <martin.lindstrom@litsec.se>
2023-11-13 18:11:17 +0100

    Deployment to sandbox

--- scripts/build-for-sandbox.sh
+++ scripts/build-for-sandbox.sh
@@ -5,7 +5,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SANDBOX_DOCKER_REPO=docker.eidastest.se:5000
-IMAGE_NAME=${SANDBOX_DOCKER_REPO}/test-my-eid
+IMAGE_NAME=${SANDBOX_DOCKER_REPO}/test-my-eid2
 
 if [ -z "$SANDBOX_DOCKER_USER" ]; then
   echo "The SANDBOX_DOCKER_USER variable must be set"
@@ -28,6 +28,5 @@ echo $SANDBOX_DOCKER_PW | docker login $SANDBOX_DOCKER_REPO -u $SANDBOX_DOCKER_U
 
 echo "Pushing image to ${SANDBOX_DOCKER_REPO} ..."
 docker push ${IMAGE_NAME}:latest
-
  
 
